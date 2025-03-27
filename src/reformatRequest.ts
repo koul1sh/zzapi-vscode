8bfb43def1b5fb335ac44a92a6acb8840e01c481
vansh845 <koulvansh845@gmail.com>
2025-03-28 01:32:42 +0530

    formdata support added

--- src/reformatRequest.ts
+++ src/reformatRequest.ts
@@ -8,26 +8,47 @@ import {
   getCurlRequest,
   replaceVariablesInRequest,
 } from "zzapi";
-
 import { getWorkingDir } from "./utils/pathUtils";
 
 import { getVarStore } from "./variables";
 
-function replaceFileContents(body: any, replaceWithFileRef?: boolean): any {
-  if (typeof body !== "string") return body;
+// function replaceFileContents(body: any, replaceWithFileRef?: boolean): any {
+//   if (typeof body !== "string"){
+//     return replaceFileMultiPart;
+//   }
 
+//   /*
+//   finds all file:// instances with atleast 1 succeeding word character
+//   matches the file-name referred to by this instance
+//   */
+//   const fileRegex = /file:\/\/([^\s]+)/g;
+//   return body.replace(fileRegex, (match, givenFilePath) => {
+//     if (match !== body) return match; // we only perform a replacement if file:// is the ENTIRE body
+
+//     if (replaceWithFileRef === true) return `@${givenFilePath}`;
+//     const filePath = path.resolve(getWorkingDir(), givenFilePath);
+//     return fs.readFileSync(filePath, "utf-8");
+//   });
+// }
+
+function getFileFromPath(pathName : string) {
   /*
   finds all file:// instances with atleast 1 succeeding word character
   matches the file-name referred to by this instance
   */
   const fileRegex = /file:\/\/([^\s]+)/g;
-  return body.replace(fileRegex, (match, givenFilePath) => {
-    if (match !== body) return match; // we only perform a replacement if file:// is the ENTIRE body
+  if(fileRegex.test(pathName)){
+    console.log("pathname",pathName)
+    const filePath = pathName.replace(fileRegex, (match, givenFilePath) => {
+      if (match !== pathName) return match; // we only perform a replacement if file:// is the ENTIRE body
+  
+      return path.resolve(getWorkingDir(), givenFilePath);
+    });
+    return filePath;
+  }else{
+    return pathName;
+  }
 
-    if (replaceWithFileRef === true) return `@${givenFilePath}`;
-    const filePath = path.resolve(getWorkingDir(), givenFilePath);
-    return fs.readFileSync(filePath, "utf-8");
-  });
 }
 
 function formatAndReplaceVars(
@@ -41,10 +62,44 @@ function formatAndReplaceVars(
   if (request.httpRequest.body && typeof request.httpRequest.body == "object")
     autoHeaders["content-type"] = "application/json";
   request.httpRequest.headers = Object.assign(autoHeaders, request.httpRequest.headers);
-
-  request.httpRequest.body = replaceFileContents(request.httpRequest.body, curlReq);
+  request.httpRequest.body = replaceFileMultiPart(request.httpRequest.body);
   return replaceVariablesInRequest(request, getVarStore().getAllVariables());
 }
+
+
+
+export function replaceFileMultiPart(body? : any){
+  if(Array.isArray(body)){
+    for (let i = 0; i < body.length; i++) {
+      body[i] = getFileFromPath(body[i])
+    }
+  }else if(typeof body == 'object'){
+    for (const key in body) {
+      if (Object.prototype.hasOwnProperty.call(body, key)) {
+        const element = body[key];
+        if(typeof element == "string"){
+          body[key] = getFileFromPath(element)
+        }
+        else if(Array.isArray(element)){
+          replaceFileMultiPart(element)
+        }
+      }
+    }
+  }else if(typeof body == 'string'){
+      /*
+  finds all file:// instances with atleast 1 succeeding word character
+  matches the file-name referred to by this instance
+  */
+  const fileRegex = /file:\/\/([^\s]+)/g;
+  return body.replace(fileRegex, (match, givenFilePath) => {
+    if (match !== body) return match; // we only perform a replacement if file:// is the ENTIRE body
+    const filePath = path.resolve(getWorkingDir(), givenFilePath);
+    return fs.readFileSync(filePath, "utf-8");
+  });
+  }
+  return body;
+}
+
 
 export function getGotRequest(
   request: RequestSpec,
